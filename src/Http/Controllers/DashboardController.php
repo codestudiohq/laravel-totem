4734a348f9a76b203f3441642b232444c99483ff
Gabriel Caruso <carusogabriel34@gmail.com>
2017-08-25 09:19:51 -0300

    Create PHPDocs for DashboardController

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -4,6 +4,11 @@ namespace Studio\Totem\Http\Controllers;
 
 class DashboardController extends Controller
 {
+    /**
+     * Single page application catch-all route.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         return redirect()->route('totem.tasks.all');
