e7e2a1cf7fcc1606f0fcf56f8b2cce139667c657
Quentin Schmick <qschmick@orisintel.com>
2017-11-15 01:41:08 -0500

    Added new import/export routes; Updated index.blade to show import/export options; Updated TasksController to export Tasks to json and allow importing json

--- src/Http/Controllers/TasksController.php
+++ src/Http/Controllers/TasksController.php
@@ -2,6 +2,7 @@
 
 namespace Studio\Totem\Http\Controllers;
 
+use Studio\Totem\Frequency;
 use Studio\Totem\Task;
 use Studio\Totem\Totem;
 use Studio\Totem\Contracts\TaskInterface;
@@ -126,5 +127,73 @@ class TasksController extends Controller
         return redirect()
             ->route('totem.tasks.all')
             ->with('success', trans('totem::messages.success.delete'));
+    }
+
+    /**
+     * JSON representation of tasks and their frequencies
+     *
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    public function export()
+    {
+        return response($this->tasks->findAll()->toJson(), 200, [
+            'Content-Type' => 'application/json',
+            'Content-Disposition' => 'attachment; filename="totem_tasks.json"',
+        ]);
+    }
+
+    /**
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function import()
+    {
+        $errors = [];
+        $records_imported = 0;
+        if (request()->hasFile('tasks')) {
+            $file = request()->file('tasks');
+            if(ends_with($file->getClientOriginalName(), '.json')) {
+                try {
+                    $data = json_decode(file_get_contents($file->getPathname()));
+                    foreach ($data as $record) {
+                        $task = Task::updateOrCreate([
+                            'id' => $record->id,
+                        ], [
+                            'description' => $record->description,
+                            'command' => $record->command,
+                            'parameters' => $record->parameters,
+                            'expression' => $record->expression,
+                            'timezone' => $record->timezone,
+                            'is_active' => $record->is_active,
+                            'dont_overlap' => $record->dont_overlap,
+                            'run_in_maintenance' => $record->run_in_maintenance,
+                            'notification_email_address' => $record->notification_email_address,
+                            'notification_phone_number' => $record->notification_phone_number,
+                            'notification_slack_webhook' => $record->notification_slack_webhook,
+                        ]);
+
+                        if (!empty($record->frequencies)) {
+                            foreach ($record->frequencies as $freq) {
+                                Frequency::updateOrCreate([
+                                    'id' => $freq->id,
+                                ], [
+                                    'task_id' => $task->id,
+                                    'label' => $freq->label,
+                                    'interval' => $freq->interval,
+                                ]);
+                            }
+                        }
+                        $records_imported++;
+                    }
+                } catch (\Exception $ex) {
+                    $errors[] = 'An error occurred while importing data.';
+                }
+            }
+        }
+
+        if($records_imported == 0) {
+            $errors[] = 'Invalid data or no record selected.';
+        }
+
+        return redirect(route('totem.tasks.all'))->withErrors($errors);
     }
 }
