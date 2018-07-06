f2a06e0b7964d4012b1b07360ed49a4ce0b60b02
Quentin Schmick <qschmick@orisintel.com>
2018-07-06 17:32:33 -0400

    Unique index names

--- database/migrations/2018_07_06_165603_add_indexes_for_tasks.php
+++ database/migrations/2018_07_06_165603_add_indexes_for_tasks.php
@@ -14,25 +14,25 @@ class AddIndexesForTasks extends Migration
     public function up()
     {
         Schema::table(config('totem.table_prefix').'task_results', function (Blueprint $table) {
-            $table->index('task_id', 'task_id_idx');
-            $table->index('ran_at', 'ran_at_idx');
+            $table->index('task_id', 'task_results_task_id_idx');
+            $table->index('ran_at', 'task_results_ran_at_idx');
             $table->foreign('task_id', 'task_id_fk')
                 ->references('id')->on('tasks');
         });
 
         Schema::table(config('totem.table_prefix').'task_frequencies', function (Blueprint $table) {
-            $table->index('task_id', 'task_id_idx');
-            $table->foreign('task_id', 'task_id_fk')
+            $table->index('task_id', 'task_frequencies_task_id_idx');
+            $table->foreign('task_id', 'task_frequencies_task_id_fk')
                 ->references('id')->on('tasks');
         });
 
         Schema::table(config('totem.table_prefix').'tasks', function (Blueprint $table) {
-            $table->index('is_active', 'is_active_idx');
-            $table->index('dont_overlap', 'dont_overlap_idx');
-            $table->index('run_in_maintenance', 'run_in_maintenance_idx');
-            $table->index('run_on_one_server', 'run_on_one_server_idx');
-            $table->index('auto_cleanup_num', 'auto_cleanup_num_idx');
-            $table->index('auto_cleanup_type', 'auto_cleanup_type_idx');
+            $table->index('is_active', 'tasks_is_active_idx');
+            $table->index('dont_overlap', 'tasks_dont_overlap_idx');
+            $table->index('run_in_maintenance', 'tasks_run_in_maintenance_idx');
+            $table->index('run_on_one_server', 'tasks_run_on_one_server_idx');
+            $table->index('auto_cleanup_num', 'tasks_auto_cleanup_num_idx');
+            $table->index('auto_cleanup_type', 'tasks_auto_cleanup_type_idx');
         });
     }
 
@@ -44,23 +44,23 @@ class AddIndexesForTasks extends Migration
     public function down()
     {
         Schema::table(config('totem.table_prefix').'task_results', function (Blueprint $table) {
-            $table->dropIndex('task_id_idx');
-            $table->dropIndex('ran_at_idx');
-            $table->dropForeign('task_id_fk');
+            $table->dropIndex('task_results_task_id_idx');
+            $table->dropIndex('task_results_ran_at_idx');
+            $table->dropForeign('task_results_task_id_fk');
         });
 
         Schema::table(config('totem.table_prefix').'task_frequencies', function (Blueprint $table) {
-            $table->dropIndex('task_id_idx');
-            $table->dropForeign('task_id_fk');
+            $table->dropIndex('task_frequencies_task_id_idx');
+            $table->dropForeign('task_frequencies_task_id_fk');
         });
 
         Schema::table(config('totem.table_prefix').'tasks', function (Blueprint $table) {
-            $table->dropIndex('is_active_idx');
-            $table->dropIndex('dont_overlap_idx');
-            $table->dropIndex('run_in_maintenance_idx');
-            $table->dropIndex('run_on_one_server_idx');
-            $table->dropIndex('auto_cleanup_num_idx');
-            $table->dropIndex('auto_cleanup_type_idx');
+            $table->dropIndex('tasks_is_active_idx');
+            $table->dropIndex('tasks_dont_overlap_idx');
+            $table->dropIndex('tasks_run_in_maintenance_idx');
+            $table->dropIndex('tasks_run_on_one_server_idx');
+            $table->dropIndex('tasks_auto_cleanup_num_idx');
+            $table->dropIndex('tasks_auto_cleanup_type_idx');
         });
     }
 }
