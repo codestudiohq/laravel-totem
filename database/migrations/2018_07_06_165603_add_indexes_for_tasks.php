5100a6cbcb5a9da75ff87b614e7a867b395716a1
Quentin Schmick <qschmick@orisintel.com>
2018-07-06 17:09:14 -0400

    Fixing style issues

--- database/migrations/2018_07_06_165603_add_indexes_for_tasks.php
+++ database/migrations/2018_07_06_165603_add_indexes_for_tasks.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class AlterTasksTableAddAutoCleanupNumAndTypeFields extends Migration
 {
@@ -13,20 +13,20 @@ class AlterTasksTableAddAutoCleanupNumAndTypeFields extends Migration
      */
     public function up()
     {
-        Schema::table(config('totem.table_prefix') . 'task_results', function (Blueprint $table) {
+        Schema::table(config('totem.table_prefix').'task_results', function (Blueprint $table) {
             $table->index('task_id', 'task_id_idx');
             $table->index('ran_at', 'ran_at_idx');
             $table->foreign('task_id', 'task_id_fk')
                 ->references('task_id')->on('tasks');
         });
 
-        Schema::table(config('totem.table_prefix') . 'task_frequencies', function (Blueprint $table) {
+        Schema::table(config('totem.table_prefix').'task_frequencies', function (Blueprint $table) {
             $table->index('task_id', 'task_id_idx');
             $table->foreign('task_id', 'task_id_fk')
                 ->references('task_id')->on('tasks');
         });
 
-        Schema::table(config('totem.table_prefix') . 'tasks', function (Blueprint $table) {
+        Schema::table(config('totem.table_prefix').'tasks', function (Blueprint $table) {
             $table->index('is_active', 'is_active_idx');
             $table->index('dont_overlap', 'dont_overlap_idx');
             $table->index('run_in_maintenance', 'run_in_maintenance_idx');
@@ -43,18 +43,18 @@ class AlterTasksTableAddAutoCleanupNumAndTypeFields extends Migration
      */
     public function down()
     {
-        Schema::table(config('totem.table_prefix') . 'task_results', function (Blueprint $table) {
+        Schema::table(config('totem.table_prefix').'task_results', function (Blueprint $table) {
             $table->dropIndex('task_id_idx');
             $table->dropIndex('ran_at_idx');
             $table->dropForeign('task_id_fk');
         });
 
-        Schema::table(config('totem.table_prefix') . 'task_frequencies', function (Blueprint $table) {
+        Schema::table(config('totem.table_prefix').'task_frequencies', function (Blueprint $table) {
             $table->dropIndex('task_id_idx');
             $table->dropForeign('task_id_fk');
         });
 
-        Schema::table(config('totem.table_prefix') . 'tasks', function (Blueprint $table) {
+        Schema::table(config('totem.table_prefix').'tasks', function (Blueprint $table) {
             $table->dropIndex('is_active_idx');
             $table->dropIndex('dont_overlap_idx');
             $table->dropIndex('run_in_maintenance_idx');
