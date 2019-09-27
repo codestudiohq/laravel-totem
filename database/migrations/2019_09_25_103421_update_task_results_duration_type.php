d7582aee34d4963c85313efcbdd2e12e031cf5fe
Quentin Schmick <qschmick@orisintel.com>
2019-09-27 10:15:08 -0400

    Added migration to move duration from varchar to decimal (#187)
    * Added migration to move duration from varchar to decimal
    
    * Added doctrine dbal requirement
    
    * Updated from double to decimal

---
+++ database/migrations/2019_09_25_103421_update_task_results_duration_type.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Studio\Totem\Database\TotemMigration;
+
+class UpdateTaskResultsDurationType extends TotemMigration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection(TOTEM_DATABASE_CONNECTION)
+            ->table(TOTEM_TABLE_PREFIX.'task_results', function (Blueprint $table) {
+                $table->decimal('duration', 24, 14)->change();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection(TOTEM_DATABASE_CONNECTION)
+            ->table(TOTEM_TABLE_PREFIX.'task_results', function (Blueprint $table) {
+                $table->string('duration', 255)->change();
+            });
+    }
+}
