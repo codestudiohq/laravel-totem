91f32775f5f82ca2ae3cacf336349d92bfc6038e
b-roberts <b-roberts@users.noreply.github.com>
2019-11-08 19:29:54 -0600

    [feature] allows publishing views
    - adds the ability to publish default views and override them in a Laravel project

--- src/Providers/TotemServiceProvider.php
+++ src/Providers/TotemServiceProvider.php
@@ -2,17 +2,16 @@
 
 namespace Studio\Totem\Providers;
 
-use Studio\Totem\Totem;
 use Cron\CronExpression;
 use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
-use Studio\Totem\Contracts\TaskInterface;
+use Illuminate\Support\ServiceProvider;
 use Studio\Totem\Console\Commands\ListSchedule;
 use Studio\Totem\Console\Commands\PublishAssets;
+use Studio\Totem\Contracts\TaskInterface;
 use Studio\Totem\Repositories\EloquentTaskRepository;
+use Studio\Totem\Totem;
 
 class TotemServiceProvider extends ServiceProvider
 {
@@ -109,6 +108,10 @@ class TotemServiceProvider extends ServiceProvider
         $this->publishes([
             TOTEM_PATH.'/public/img' => public_path('vendor/totem/img'),
         ], 'totem-assets');
+
+        $this->publishes([
+            TOTEM_PATH.'/resources/views' => resource_path('views/vendor/totem'),
+        ], 'totem-views');
 
         $this->publishes([
             TOTEM_PATH.'/config' => config_path(),
