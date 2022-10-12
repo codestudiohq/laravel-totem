a010131d459693f704678a6596cc404f8f14a42e
陈瑞华 <46486133+ChenRuihua@users.noreply.github.com>
2022-10-12 19:17:10 +0800

    Update 2022_03_14_120000_alter_task_results_table_add_index_on_created_at.php
    fix the wrong index name

--- database/migrations/2022_03_14_120000_alter_task_results_table_add_index_on_created_at.php
+++ database/migrations/2022_03_14_120000_alter_task_results_table_add_index_on_created_at.php
@@ -28,7 +28,7 @@ class AlterTaskResultsTableAddIndexOnCreatedAt extends TotemMigration
     {
         Schema::connection(TOTEM_DATABASE_CONNECTION)
             ->table(TOTEM_TABLE_PREFIX.'task_results', function (Blueprint $table) {
-                $table->dropIndex('created_at');
+                $table->dropIndex(TOTEM_TABLE_PREFIX.'task_results_created_at_index');
             });
     }
 }
